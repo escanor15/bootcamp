e2385fc5c9b152aa9c30741efa2cca6ed9da57d6
escanor15 <49676420+escanor15@users.noreply.github.com>
2024-01-30 17:14:57 +0700

    tugas

---
+++ sort.js
@@ -0,0 +1,27 @@
+var umur = [28,19,23,58,19,30,50,29];
+
+// function sortArray(){
+//     var temp = 0;
+//     for(var i = 0; i<umur.length; i++){
+        
+//         for(var j = 0; j<umur.length; j++){
+//             if(umur[i] < umur[i+1]){
+//                 temp = umur[i];
+//             } else {
+//                 temp = umur[i+1];
+//             }
+//         }
+//         console.log(temp); 
+//     }
+// }
+
+function sortArray(){
+    var arr = umur.sort();
+    var temp = 0;
+    var finalArr = [];
+    for(var i = 0; i<arr.length; i++){
+        if()
+    }
+    console.log(temp); 
+
+}
